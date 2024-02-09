f35eea8a8b9bd48fd520dea6f6fd2788337dc37d
SumandeepK2004 <157737058+SumandeepK2004@users.noreply.github.com>
2024-02-09 18:20:10 -0500

    Add files via upload

---
+++ subscribe.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="https://marketplace.canva.com/EAEv8zsxbAw/1/0/1600w/canva-pizza-shop-restaurant-thank-you-for-your-order-card-simple-AkVYqtO6Jo8.jpg" height="700px"width="1500px">
+    
+</body>
+</html>
